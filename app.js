299228ae4258251d48a876c99cf4207cf72975d3
jbpeek <jbpeek16@stlawu.edu>
2020-04-14 00:37:27 -0600

    Adds back-slash

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(function(request, response, next) {
 // Redirect from the home page
 app.get('/', function(request, response) {
   console.log("Redirecting to questions");
-  response.redirect('questions');
+  response.redirect('/questions');
 });
 
 // Route content requests
