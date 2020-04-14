15bcffdc4b059b9188b56e07f118378a9980a566
Tim <timwjones98@gmail.com>
2020-04-13 22:14:51 -0400

    set up views and display user data

--- controller/users.js
+++ controller/users.js
@@ -1,7 +1,8 @@
 
 // get all of the users
-module.exports.index = function(request, response) {
-  response.send('GET /users');
+module.exports.index = function(request, response, next) {
+  User.distinct('id')
+    .then(userIDS => response.redirect(`/users$`))
 };
 
 // get a single user
