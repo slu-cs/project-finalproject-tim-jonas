ff764dc8cd93703c1f28b26fc7483af19aff9d99
jbpeek <jbpeek16@stlawu.edu>
2020-04-13 23:41:31 -0600

    Removes uneccessary line

--- router.js
+++ router.js
@@ -11,7 +11,6 @@ router.get('/questions/:user_id', questions.retrieve);
 
 // Handle user requests
 router.get('/users', users.index);
-router.get('/users/:user_id', users.retrieve);
 
 // Export the router
 module.exports = router;
